5d6de6b02d670eaffda68555189f1dda747c73b0
Michał Nowakowski <michal951@poczta.onet.pl>
2020-03-03 16:29:35 +0100

    .

--- 02-JSCoreConcepts/address.js
+++ 02-JSCoreConcepts/address.js
@@ -1,11 +1,10 @@
 title = "dr";
 name = "Mateusz";
-surname = 'Kubicki';
-street = 'Długa 15';
-city = 'Kraków';
-zip = '30-781';
-country = 'Poland';
-
+surname = "Kubicki";
+street = "Długa 15";
+city = "Kraków";
+zip = "30-781";
+country = "Poland";
 
 /* 
 dr Mateusz Kubicki
